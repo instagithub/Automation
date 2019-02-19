07f9246cd5539183b26364cde4b99c66b48b2690
berip <Pavan.Beri@in.pega.com>
2019-02-19 11:44:22 +0530

    added/modified files to add extent reports

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -12,7 +12,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions()
+@CucumberOptions(plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/extentReport.html"})
 public class RunCukesTest extends AbstractTestNGCucumberTests{
 	
 	String COPYRIGHT = "Copyright (c) 2018  Pegasystems Inc.";
