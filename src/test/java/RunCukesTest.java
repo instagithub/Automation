539c67908aaff3200fbe26b02c4d7c503b13294d
subuj <jyotiranjan.subudhi@in.pega.com>
2020-01-21 11:52:34 +0530

    pushed modified version of the base ui jar

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -3,11 +3,16 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions(plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/extentReport.html"})
+@CucumberOptions()
 public class RunCukesTest extends AbstractTestNGCucumberTests{
 	
 	String COPYRIGHT = "Copyright (c) 2018  Pegasystems Inc.";
-	String VERSION = "$Id: RunCukesTest.java 121819 2018-01-26 07:29:51Z SachinVellanki $";
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
+
 
