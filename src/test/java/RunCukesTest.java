666d57dffe315ff5695863bf570a397e9c9fa4ce
subuj <jyotiranjan.subudhi@in.pega.com>
2020-01-21 11:58:32 +0530

    reverted the changes of previous commit

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -3,16 +3,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions()
+@CucumberOptions(plugin = {"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/extentReport.html"})
 public class RunCukesTest extends AbstractTestNGCucumberTests{
 	
 	String COPYRIGHT = "Copyright (c) 2018  Pegasystems Inc.";
-	@Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+	String VERSION = "$Id: RunCukesTest.java 121819 2018-01-26 07:29:51Z SachinVellanki $";
 
 }
-
-
